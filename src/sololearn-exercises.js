c73b93837b075a490e827c1e112da42d3287ea51
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-03-03 02:05:23 +0000

    js and html files

---
+++ src/sololearn-exercises.js
@@ -0,0 +1,28 @@
+//the switch statement
+var day = 3;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+        document.write("Another day");
+}
+
+//the for loop statement
+var i = 1;
+for (k=1; k<10; k++) {
+    i += k;
+    document.write(k + "<br/>");
+}
+
+var i = 0;
+for(; i<10; ) {
+    document.write(i);
+    i++;
+}
