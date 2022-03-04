0bf20e565459c9f4c2c3327fc6f40169b4b13830
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-03-04 01:26:49 +0000

    loops while + continue statement

---
+++ src/dowhileloop.js
@@ -0,0 +1,14 @@
+//do while loop
+var count = 1;
+do {
+    document.write("hello <br/>");
+    count++;
+}
+while (count<=10);
+
+//while loop 
+var x = 0;
+while(x<6) {
+    x++;
+}
+document.write(x);
